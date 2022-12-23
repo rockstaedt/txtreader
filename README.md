81106d09595cd3f7c0d9f10e8374cc9227aa915a
Eric Rockstädt <eric_rockstaedt@yahoo.de>
2022-12-23 16:34:22 +0100

    Init project

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # txtreader
+
+[![build](https://github.com/rockstaedt/txtreader/actions/workflows/CI.yml/badge.svg)](https://github.com/rockstaedt/txtreader/actions/workflows/CI.yml)
+[![codecov](https://codecov.io/gh/rockstaedt/txtreader/branch/main/graph/badge.svg?token=VW245SMVP5)](https://codecov.io/gh/rockstaedt/txtreader)
+[![Latest tag](https://img.shields.io/github/v/tag/rockstaedt/txtreader)](https://github.com/rockstaedt/txtreader/releases)
+
 A Golang package to read lines from a .txt file
---
+++ filereader_test.go
@@ -0,0 +1,22 @@
+package utils
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestGetLinesFromTextFile(t *testing.T) {
+	t.Run("returns lines slice from text file", func(t *testing.T) {
+		lines, _ := GetLinesFromTextFile("testdata/test.txt")
+
+		assert.Len(t, lines, 3)
+		assert.Equal(t, "Hello", lines[0])
+		assert.Equal(t, "World", lines[len(lines)-1])
+	})
+
+	t.Run("returns err if file was not found", func(t *testing.T) {
+		_, err := GetLinesFromTextFile("not_existing_file")
+
+		assert.Error(t, err)
+	})
+}
